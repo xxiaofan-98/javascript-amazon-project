3aacd3c98499fef8d2d54b2b086694183f2afd2d
Xiaofan Liu <138967508+xxiaofan-98@users.noreply.github.com>
2023-11-20 14:57:10 -0500

    update time function by radio button

--- data/cart.js
+++ data/cart.js
@@ -54,3 +54,16 @@ export function removeFromCart(productId) {
     saveToStorage();
 };
 
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((item) => {
+        if (productId === item.productId) {
+            matchingItem = item;
+        }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    saveToStorage();
+}
